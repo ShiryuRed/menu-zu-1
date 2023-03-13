80edb5758aa8f78cb3ad7e4dacc8a16180a60483
ShiryuRed <62433632+ShiryuRed@users.noreply.github.com>
2023-03-13 16:55:34 -0600

    Add files via upload

--- js/open-menu__script.js
+++ js/open-menu__script.js
@@ -1,6 +1,17 @@
 
-const comidas = document.querySelector(".container-menu-comidas");
 
-function abrir() {
-	comidas.classList.toggle("abrir")
-}
+const menuContainer = document.querySelector(".menu-container");
+
+const menuPizzas = document.querySelector(".container-menu-pizzas");
+const openMenuP = document.querySelector(".switch-food")
+
+openMenuP.addEventListener("click", ()=>{
+	menuPizzas.classList.toggle("abrir");
+})
+
+const menuHamburguesas = document.querySelector(".container-menu-hamburguesas");
+const openMenuH = document.querySelector(".switch-ham")
+
+openMenuH.addEventListener("click", ()=>{
+	menuHamburguesas.classList.toggle("abrir");
+})
