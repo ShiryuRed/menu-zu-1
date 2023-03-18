5fe15c9a3d08a0123c8ab56bd1651db0641cb69a
ShiryuRed <62433632+ShiryuRed@users.noreply.github.com>
2023-03-17 23:32:15 -0600

    Add files via upload

--- js/open-menu__script.js
+++ js/open-menu__script.js
@@ -6,35 +6,44 @@ const menuPizzas = document.querySelector(".container-menu-pizzas");
 const openMenuP = document.querySelector(".switch-food");
 
 openMenuP.addEventListener("click", ()=>{
-	menuPizzas.classList.toggle("abrir");
+	menuPizzas.classList.toggle("abrir-pizza");
+	menuPizzas.classList.toggle("ocultar-pizzas");
+	menuPizzas.classList.remove("abrir-pizza-calzone");
+	pizza.classList.add("p-focus");
+	calzone.classList.remove("p-focus");
+	calzoneMenu.classList.remove("calzone-menu-open");
 })
 
 const menuHamburguesas = document.querySelector(".container-menu-hamburguesas");
 const openMenuH = document.querySelector(".switch-ham");
 
 openMenuH.addEventListener("click", ()=>{
-	menuHamburguesas.classList.toggle("abrir");
+	menuHamburguesas.classList.toggle("abrir-hamburguesas");
+	menuHamburguesas.classList.toggle("ocultar-hamburguesas");
 })
 
 const menuComplementos = document.querySelector(".container-menu-complementos");
 const openMenuC = document.querySelector(".switch-com");
 
 openMenuC.addEventListener("click", ()=>{
-	menuComplementos.classList.toggle("abrir");
+	menuComplementos.classList.toggle("abrir-complementos");
+	menuComplementos.classList.toggle("ocultar-complementos");
 })
 
 const menuBebidas = document.querySelector(".container-menu-bebidas");
 const openMenuB = document.querySelector(".switch-beb");
 
 openMenuB.addEventListener("click", ()=>{
-	menuBebidas.classList.toggle("abrir");
+	menuBebidas.classList.toggle("abrir-bebidas");
+	menuBebidas.classList.toggle("ocultar-bebidas");
 })
 
 const menuEspeciales = document.querySelector(".container-menu-especial");
 const openMenuE = document.querySelector(".switch-esp");
 
 openMenuE.addEventListener("click", ()=>{
-	menuEspeciales.classList.toggle("abrir");
+	menuEspeciales.classList.toggle("abrir-especiales");
+	menuEspeciales.classList.toggle("ocultar-especiales");
 })
 
 
@@ -59,4 +68,23 @@ btnCarta.addEventListener("click", ()=> {
 			cartaContainer.setAttribute("hidden", "true");
       }, 200);
 	}
-})
+});
+
+
+
+const pizza = document.querySelector(".pizza");
+const calzone = document.querySelector(".calzone");
+const calzoneMenu = document.querySelector(".calzone-menu");
+
+pizza.addEventListener("click", ()=> {
+	calzone.classList.remove("p-focus");
+	pizza.classList.add("p-focus");
+	calzoneMenu.classList.remove("calzone-menu-open");
+	menuPizzas.classList.remove("abrir-pizza-calzone");
+});
+calzone.addEventListener("click", ()=> {
+	pizza.classList.remove("p-focus");
+	calzone.classList.add("p-focus");
+	calzoneMenu.classList.add("calzone-menu-open");
+	menuPizzas.classList.add("abrir-pizza-calzone");
+});
